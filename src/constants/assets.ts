c76992ea0e022794dfa7bb7c556c8456fa7bc6e9
twosigs <nutcrackeronsol@gmail.com>
2024-01-29 19:49:38 -0800

    Rename S3 -> SHDW

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,9 +1,9 @@
-const BASE_S3 =
+const SHDW_URL =
   "https://shdw-drive.genesysgo.net/GwJapVHVvfM4Mw4sWszkzywncUWuxxPd6s9VuFfXRgie";
 
-export const LOGO_TEXT = `${BASE_S3}/logo_text.png`;
-export const HERO_LOGO = `${BASE_S3}/long_wen.png`;
+export const LOGO_TEXT = `${SHDW_URL}/logo_text.png`;
+export const HERO_LOGO = `${SHDW_URL}/long_wen.png`;
 
-export const LOGO_SQUARE = `${BASE_S3}/logo_300.png`;
-export const POEM = `${BASE_S3}/wen_poem.jpg`;
-export const FOUNDATION_LOGO = `${BASE_S3}/wen_foundation.png`;
+export const LOGO_SQUARE = `${SHDW_URL}/logo_300.png`;
+export const POEM = `${SHDW_URL}/wen_poem.jpg`;
+export const FOUNDATION_LOGO = `${SHDW_URL}/wen_foundation.png`;
