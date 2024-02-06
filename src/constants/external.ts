bed65599260c851b7c36d7b4f1eb5305801e850a
twosigs <157307942+twosigs@users.noreply.github.com>
2024-02-06 07:46:40 -0800

    Add telegram link to navbar (#7)

--- src/constants/external.ts
+++ src/constants/external.ts
@@ -8,6 +8,7 @@ export const SOLANA_EXPLORER = "https://explorer.solana.com/address/";
 export const CLAIM_AIRDROP_LINK = "https://lfg.jup.ag/wen";
 export const BUY_WEN_LINK = "https://jup.ag/swap/USDC-WEN";
 export const WEN_TWITTER_LINK = "https://x.com/wenwencoin";
+export const WEN_TELEGRAM_LINK = "https://t.me/wenwencoinsol";
 export const BIRDEYE_LINK = `https://birdeye.so/token/${WEN_MINT}?chain=solana`;
 export const POEM_VAULT_LINK =
   "https://www.ovols.com/vault/WENWENvqqNya429ubCdR81ZmD69brwQaaBYY6p3LCpk/";
