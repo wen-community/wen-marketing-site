b9ab0350c1ea76d7b2f6b91bd2e4a1cc26b938ed
Balmy Gazebo <balmy.gazebo_0f@icloud.com>
2024-06-07 11:17:09 -0400

    fixed wpl route

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -5,14 +5,14 @@ import {
   Routes as RouterRoutes,
 } from "react-router-dom";
 import { Home, Wns } from "./pages";
-import { HOME_SLUG, WNS_SLUG } from "../constants";
+import { HOME_SLUG, WPL_SLUG } from "../constants";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <RouterRoutes>
         <Route path={HOME_SLUG} element={<Home />} />
-        <Route path={WNS_SLUG} element={<Wns />} />
+        <Route path={WPL_SLUG} element={<Wns />} />
 
         <Route path="*" element={<Navigate to="/" />} />
       </RouterRoutes>
