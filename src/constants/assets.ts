d04ffe487ddb810a5b4c213bdc6a8b1bae3ad5bc
Balmy Gazebo <155618733+balmy-gazebo@users.noreply.github.com>
2024-11-12 12:04:00 -0500

    Update Partner Logos (#17)

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -9,14 +9,14 @@ export const FOUNDATION_LOGO = `https://i.imgur.com/dLevFYd.png`;
 export const ELEMENTS_LOGO =
   "https://pbs.twimg.com/media/GGLsAmzWoAA4nP5?format=jpg&name=large";
 
-export const ASSET_DASH_LOGO = `${SHDW_URL}/asset-dash-logo.svg`;
-export const JUPITER_LOGO = `${SHDW_URL}/jupiter-logo.svg`;
-export const MAGIC_EDEN_LOGO = `${SHDW_URL}/magic-eden-logo.svg`;
-export const OVOLS_LOGO = `${SHDW_URL}/ovols-logo.svg`;
-export const PHANTOM_LOGO = `${SHDW_URL}/phantom-logo.svg`;
-export const TENSOR_LOGO = `${SHDW_URL}/tensor-logo.svg`;
-export const HELIUS_LOGO = `${SHDW_URL}/helius-logo.svg`;
-export const BACKPACK_LOGO = `${SHDW_URL}/backpack-logo.svg`;
+export const ASSET_DASH_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/b894c8454cabe65e7b0d86f06d700b0d/raw/8876a8f8116e7f4d80d0968a85317796ea302638/asset-dash-logo.svg`;
+export const JUPITER_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/9a57b1a25f121296a84c8e9125459d3e/raw/dfe774f0d379faebbfd85f4859cd4eb071538c3f/jupiter-logo.svg`;
+export const MAGIC_EDEN_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/edbf679160cadef56c97dc3ddc9102b0/raw/07820c1c3ce837df2071a1b5f6ea9c3459ffdeea/magic-eden-logo.svg`;
+export const OVOLS_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/3171d0a26de0112dc7075b1e9d8b899c/raw/64f7b56984e957313de9dd3240587ec3129399c1/ovols-logo.svg`;
+export const PHANTOM_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/58a8ea78bf33bcdf23216f2a1b137643/raw/dea26401409d527ef8a8ecd86401f66a550feb26/phantom-logo.svg`;
+export const TENSOR_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/5bc5a34bc518914ab4483fbe78d9b1cf/raw/99d2c200a4f0726537d4e9427e16cb4ae674dcf5/tensor-logo.svg`;
+export const HELIUS_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/47bdc34e2b9fdca6203a17292a585199/raw/877629184d8c625775ca76e16c317c5ce40e06cd/helius-logo.svg`;
+export const BACKPACK_LOGO = `https://raw.githubusercontent.com/gist/balmy-gazebo/8dd1e1751b7dd5cea467ba8dc06073d5/raw/273a33bcd8aac0dfdb90df0169e32cfd1b762470/backpack-logo.svg`;
 export const SOLANA_FOUNDATION_LOGO =
   "https://solana.org/_next/static/media/logo-wordmark-light.a56a29ac.svg";
 
