8a510b0cd45b3160a89c3c883b8a5cd03c718f1e
Balmy Gazebo <155618733+balmy-gazebo@users.noreply.github.com>
2024-11-12 11:26:35 -0500

    Update WEN foundation images
    SHDW link broken.

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,14 +1,11 @@
-const SHDW_URL =
-  "https://shdw-drive.genesysgo.net/GwJapVHVvfM4Mw4sWszkzywncUWuxxPd6s9VuFfXRgie";
+export const LOGO_TEXT = `https://i.imgur.com/yHk30mV.png`;
+export const HERO_LOGO = `https://i.imgur.com/lIP3ODk.png`;
 
-export const LOGO_TEXT = `${SHDW_URL}/logo_text.png`;
-export const HERO_LOGO = `${SHDW_URL}/long_wen.png`;
+export const LOGO_SQUARE = `https://i.imgur.com/p71zrm9.png`;
+export const LOGO_CAT = `https://i.imgur.com/MboddUi.png`;
 
-export const LOGO_SQUARE = `${SHDW_URL}/logo_300.png`;
-export const LOGO_CAT = `${SHDW_URL}/wen_head.png`;
-
-export const POEM = `${SHDW_URL}/wen_poem.jpg`;
-export const FOUNDATION_LOGO = `${SHDW_URL}/wen_foundation.png`;
+export const POEM = `https://i.imgur.com/cmB9AUk.jpeg`;
+export const FOUNDATION_LOGO = `https://i.imgur.com/dLevFYd.png`;
 export const ELEMENTS_LOGO =
   "https://pbs.twimg.com/media/GGLsAmzWoAA4nP5?format=jpg&name=large";
 
